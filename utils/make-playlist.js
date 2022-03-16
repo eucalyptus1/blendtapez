744fa964917272892c577705ebc37dc24cba21b9
euc1 <jamesharrington585@hotmail.com>
2022-03-16 18:40:01 -0400

    added more functionality to the preview div although it has a lot of work left

--- utils/make-playlist.js
+++ utils/make-playlist.js
@@ -1,36 +1,77 @@
+//playlist and track arrays
+var playlists = [];
+var tracks = [];
+var trackAmount = 0;
+var playlistAmount = 0;
 
-var trackListEl = document.querySelector("#track-list");
-var trackButtonEl = document.querySelector("button");
-var videoEl = document.querySelector("#video-player");
+// Input form variables
+var trackButtonEl = document.querySelector("#track-submit");
 var formEl = document.querySelector("#form-el");
 
+// Created playlist variables
+var playlistEl = document.querySelector("#created-playlist");
+var playlistTitle = document.querySelector("#playlist-title");
+var trackListEl = document.querySelector("#track-list");
+var trackNameEl = document.querySelector("#track-name-el");
+var videoEl = document.querySelector("#video-player");
 
-
-
+// Register form input for playlist name, track name and link
 var addTrack = function(event) {
   event.preventDefault();
+  // var playlistForm = document.getElementById("input[name='playlist-name']").value;
   var trackNameInput = document.querySelector("input[name='track-name']").value;
   var trackLinkInput = document.querySelector("input[name='track-link']").value;
+  console.log(trackNameInput);
   console.log(trackLinkInput);
+
+
+  // if (!playlistForm) {
+  //   alert("You need to enter a Playlist name");
+  //   return false;
+  // } else {
+  //   playlistTitle.innerHTML = playlistForm;
+  // }
+
+  // document.querySelector("input[name='track-link']").value = "";
+  // document.querySelector("input[name='track-name']").value = "";
+
+
 
   getVideo();
 };
 
+// initiate api call to gather data and render it on page
 function getVideo() {
-    var trackLinkInput = document.querySelector("input[name='track-link']").value;
-    const api = `https://www.youtube.com/oembed?format=json&url=${trackLinkInput}`
+  //  var playlistForm = document.getElementById("input[name='playlist-name']").value;
+   var trackNameInput = document.querySelector("input[name='track-name']").value;
+   var trackLinkInput = document.querySelector("input[name='track-link']").value;
+   const api = `https://www.youtube.com/oembed?format=json&url=${trackLinkInput}`
 
     fetch(`${api}`)
     .then(function(response) {
     response.json()
     .then(function(data) {
     console.log(data);
+    //take video embed data
     var vid = data.html;
-    var title = data.title;
-    console.log(vid);
-    console.log(title);
     videoEl.innerHTML = `${vid}`;
+    //take video title data
+    var title = data.title;
+    var link = data.author_url;
+    if (!trackNameInput) {
+        var track = document.createElement("li");
+        track.setAttribute("track-id", trackAmount);
+        track.setAttribute('href', `${link}`);
+        track.innerHTML = `${title}`;
+        trackListEl.appendChild(track);
+    } else {
+      trackNameEl.innerHTML = trackNameInput;
+      track.setAttribute("track-id", trackAmount);
+      track.setAttribute('href', `${link}`);
+      trackListEl.appendChild(track);
+    }
     }) 
+    
   })
 };
 
@@ -44,11 +85,6 @@ function getVideo() {
 //       options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
 //   }
 // });
-
-
-// function displayVideo (vid) {
-//   videoEl.innerHTML = `${vid}`;
-// };
 
 
 
