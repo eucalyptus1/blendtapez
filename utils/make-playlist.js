2b3518a1ddadf4c7663575507e474519ecba2795
euc1 <jamesharrington585@hotmail.com>
2022-03-20 19:55:07 -0400

    finished remaining functions such as playlist objects, ability to name tracks and the submit button for transfering to local storage

--- utils/make-playlist.js
+++ utils/make-playlist.js
@@ -1,12 +1,13 @@
-//playlist and track arrays
-var playlists = [];
-var tracks = [];
-var trackAmount = 0;
-var playlistAmount = 0;
+//playlist arrays
+var playlist = [];
+var playlistIdCounter = 0;
+ 
+var trackIdCounter = 0;
 
 // Input form variables
 var trackButtonEl = document.querySelector("#track-submit");
 var formEl = document.querySelector("#form-el");
+var playlistSubmit = document.querySelector("#playlist-submit");
 
 // Created playlist variables
 var playlistEl = document.querySelector("#created-playlist");
@@ -15,36 +16,42 @@ var trackListEl = document.querySelector("#track-list");
 var trackNameEl = document.querySelector("#track-name-el");
 var videoEl = document.querySelector("#video-player");
 
+//image variables
+var img1 = document.querySelector("#img1");
+var img2 = document.querySelector("#img2");
+var img3 = document.querySelector("#img3");
+var img4 = document.querySelector("#img4");
+
+
+
 // Register form input for playlist name, track name and link
 var addTrack = function(event) {
   event.preventDefault();
-  // var playlistForm = document.getElementById("input[name='playlist-name']").value;
+  var playlistFormInput = document.querySelector("input[name='playlist-name']").value;
   var trackNameInput = document.querySelector("input[name='track-name']").value;
   var trackLinkInput = document.querySelector("input[name='track-link']").value;
+  //console.logs for testing
   console.log(trackNameInput);
   console.log(trackLinkInput);
+  console.log(playlistFormInput);
 
-
-  // if (!playlistForm) {
-  //   alert("You need to enter a Playlist name");
-  //   return false;
-  // } else {
-  //   playlistTitle.innerHTML = playlistForm;
-  // }
-
-  // document.querySelector("input[name='track-link']").value = "";
-  // document.querySelector("input[name='track-name']").value = "";
-
-
+  if (!playlistFormInput) {
+    alert("You need to enter a Playlist name");
+    return false;
+  } else {
+    playlistTitle.innerHTML = playlistFormInput;
+    playlist.push(playlistFormInput);
+  }
 
   getVideo();
 };
 
 // initiate api call to gather data and render it on page
 function getVideo() {
-  //  var playlistForm = document.getElementById("input[name='playlist-name']").value;
+  //  var playlistFormInput = document.getElementById("input[name='playlist-name']").value;
    var trackNameInput = document.querySelector("input[name='track-name']").value;
    var trackLinkInput = document.querySelector("input[name='track-link']").value;
+   var playlist = [];
    const api = `https://www.youtube.com/oembed?format=json&maxwidth=400&maxheight=260&url=${trackLinkInput}`
 
     fetch(`${api}`)
@@ -59,28 +66,65 @@ function getVideo() {
     var title = data.title;
     var link = data.author_url;
     if (!trackNameInput) {
-        var track = document.createElement("li");
-        // track.setAttribute("track-id", trackAmount);
-        // track.setAttribute('href', `${link}`);
-        track.setAttribute('class', 'is-4');
-        track.innerText = `${title}`;
-        trackListEl.appendChild(track);
-
-    } else {
-      trackNameEl.innerHTML = trackNameInput;
-      track.setAttribute("track-id", trackAmount);
-      track.setAttribute('href', `${link}`);
-      trackListEl.appendChild(track);
-    }
+    var trackObj = {
+      name: `${title}`,
+      link: `${link}`
+    };
+  } else {
+    var trackObj = {
+      name: trackNameInput,
+      link: `${link}`
+    };
+  }
+    console.log(trackObj);
+    createTrackEl(trackObj);
+    
     }) 
      document.querySelector("input[name='track-link']").value = "";
      document.querySelector("input[name='track-name']").value = "";
   })
 };
 
+// function to append tracks into preview div and create unique properties before submit
+function createTrackEl (trackObj) {
+  var trackNameInput = document.querySelector("input[name='track-name']").value;
+  var trackLinkInput = document.querySelector("input[name='track-link']").value;
 
+  if (!trackNameInput) {
+    var track = document.createElement("li");
+    var a = document.createElement("a");
+    a.textContent = trackObj.name;
+    a.setAttribute('href', trackObj.link);
+    a.setAttribute('id', `${trackIdCounter}`);
+    a.classList.add('song-title');
+    trackIdCounter += 1;
+    track.appendChild(a);
+    trackListEl.appendChild(track);
+    playlist.push(trackObj);
+    console.log(playlist);
 
+} else {
+  var track = document.createElement("li");
+  var a = document.createElement("a");
+  a.textContent = trackNameInput;
+  a.setAttribute('href', `${link}`);
+  a.setAttribute('id', `${trackIdCounter}`);
+  a.classList.add('song-title');
+  trackIdCounter += 1;
+  track.appendChild(a);
+  trackListEl.appendChild(track);
+  playlist.push(trackObj);
+  console.log(playlist);
+}
+};
 
+// function to submit completed playlist into local storage
+function submitPlaylist(event) {
+  event.preventDefault();
+  localStorage.setItem("playlist", JSON.stringify(playlist));
+  playlistIdCounter += 1;
+  document.querySelector("input[name='track-name']").value = "";
+};
 
 
 // jQuery.ajaxPrefilter(function(options) {
@@ -91,5 +135,5 @@ function getVideo() {
 
 
 
-
+playlistSubmit.addEventListener("click", submitPlaylist);
 trackButtonEl.addEventListener("click", addTrack);
