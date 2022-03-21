62d6fbda0b0028600fce682191f7cb37502c5452
lsmith32 <lmksmith97@gmail.com>
2022-03-20 22:30:19 -0500

    profile page set up to render album(s) and username

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,7 +1,60 @@
 const router = require('express').router();
+const sequelize = require('../config/connection');
+const { Track, Album, Account, playlist } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('playlist');
+    Post.findAll({
+      attributes: [
+        'playlist',
+        'image_path',
+    ],
+    include: [
+        {
+        model: Comment,
+        attributes: ['id', 'playlist', 'image_path'],
+        include: {
+            model: Album,
+            attributes: ['id']
+        }
+        },
+    ]
+    })
+    .then(dbPostData => {
+
+    const albums = dbPostData.map(post => post.get({ plain: true }));
+
+    // pass a single post object into the homepage template
+    res.render('profile', { albums });
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'username',
+    ],
+    include: [
+        {
+        model: User,
+        attributes: ['username'],
+        },
+    ]
+    })
+    .then(dbPostData => {
+
+    const albums = dbPostData.map(post => post.get({ plain: true }));
+
+    // pass a single post object into the homepage template
+    res.render('profile', { albums });
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const routes = require('./controllers/');
+
+//Handlebars 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
