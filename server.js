ca62ca293b2848cc92bf1ccc4625e5995c677ef4
euc1 <jamesharrington585@hotmail.com>
2022-03-23 23:26:16 -0400

    established route for homepage

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/login.html'));
+  });
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
