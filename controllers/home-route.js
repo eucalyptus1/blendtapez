e19faf38b4abbf6f0e9a9dcfed90b755a4352b84
euc1 <jamesharrington585@hotmail.com>
2022-03-23 23:08:51 -0400

    add heroku

--- controllers/home-route.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').router();
-
-router.get('/', (req, res) => {
-    res.render('playlist');
-});
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,6 @@
 const express = require('express');
-const db = require('./config/connection');
-const apiRoutes = require('./routes/apiRoutes');
-const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-});
-
-// Use apiRoutes
-app.use('/api', apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
 });
