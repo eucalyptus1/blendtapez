004d5c7ecbbafdba8915bb827fd6f35cbe5df50c
euc1 <jamesharrington585@hotmail.com>
2022-03-24 00:43:46 -0400

    ugh

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+var path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/login.html'));
+    res.sendFile(path.join(__dirname, './public/login.html'));
   });
 
 app.listen(PORT, () => {
