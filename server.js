8f1ddf020b3f49eaf8952089bbe93076a9ac6008
euc1 <jamesharrington585@hotmail.com>
2022-03-23 23:13:12 -0400

    hopefully got page deployed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-});
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
