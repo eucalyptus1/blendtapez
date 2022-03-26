f29ac60362f3754ef3b59c91cbe9143c0fcc35f2
euc1 <jamesharrington585@hotmail.com>
2022-03-26 19:09:39 -0400

    test

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
