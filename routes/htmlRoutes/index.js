5ac3764712b5a6a000c9ee04f805566ed2715e11
euc1 <jamesharrington585@hotmail.com>
2022-03-26 20:02:14 -0400

    test

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -9,9 +9,9 @@ router.get('/make-a-playlist', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/make-a-playlist.html'));
 });
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/playlist.html'));
-});
+// router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/playlist.html'));
+// });
 
 
 
